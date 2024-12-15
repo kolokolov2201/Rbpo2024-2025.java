184b6a9233edf9f66c5c6a65b7bbd3591f4ae593
kolokolov2201 <egorkolokolov0888@gmail.com>
2024-12-15 14:12:39 +0300

    Commit 2

--- src/main/java/ru/mtuci/rbpo_2024_praktika/controller/LicenseTypeController.java
+++ src/main/java/ru/mtuci/rbpo_2024_praktika/controller/LicenseTypeController.java
@@ -1,17 +1,8 @@
 package ru.mtuci.rbpo_2024_praktika.controller;
 
-import ru.mtuci.rbpo_2024_praktika.request.LicenseTypeAddRequest;
-import ru.mtuci.rbpo_2024_praktika.model.LicenseType;
-import ru.mtuci.rbpo_2024_praktika.service.LicenseService;
-import ru.mtuci.rbpo_2024_praktika.service.LicenseTypeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 
 @RequiredArgsConstructor
