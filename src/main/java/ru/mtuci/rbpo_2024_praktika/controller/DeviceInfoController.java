58547705f3491c7b46004117ce81abd2e744fd1e
kolokolov2201 <egorkolokolov0888@gmail.com>
2024-12-20 19:29:24 +0300

    Commit 2

---
+++ src/main/java/ru/mtuci/rbpo_2024_praktika/controller/DeviceInfoController.java
@@ -0,0 +1,12 @@
+package ru.mtuci.rbpo_2024_praktika.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/info")
+@RestController
+public class DeviceInfoController {
+
+
+}
